870b14f73eb5e2d08174aaafe60ab884c44b6e9d
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-12-12 22:26:40 +0100

    added python snake. NOt fully workign yet

---
+++ snake.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python3
+import pygame
+import random
+
+HEIGHT = 10;
+WIDTH = 11;
+SIZE = 50
+LIGHT_GREEN = (164, 213, 98)
+DARK_GREEN = (157,207,90)
+APPLE_GREEN = (12, 194, 67)
+BLUE = (51, 118, 228)
+RED = (244, 75, 39)
+
+APPLES_COUNT = 5
+
+
+class Snake():
+  def __init__(self):
+    self.body = [(0, int(HEIGHT/2)), (1, int(HEIGHT/2)), (2, int(HEIGHT/2)), (3, int(HEIGHT/2))]
+    self.length = len(self.body)
+    self.head = self.body[self.length - 1]
+    self.tail = self.body[0]
+    
+  def grow(self):
+    pass
+  def move(self, direction: tuple, grow=False):
+    if not grow:
+      del self.body[0]
+    new_position = (self.head[0] + direction[0], self.head[1] + direction[1])
+    self.body.append(new_position)
+
+    self.head = new_position
+    self.tail = self.body[0];
+
+class Apples():
+  def __init__(self):
+    self.count = 0
+    self.position = [[0] * WIDTH] * HEIGHT
+
+  def remove_apple(self, pos):
+    self.count -= 1
+    self.position[pos[1]][pos[0]] = 0
+
+  def add_apple(self, snake):
+    self.count += 1
+    pos = [0, 0]
+    while 1:
+      idx = 0
+      while idx < 2:
+        while 1:
+          pos[idx] = random.randint(0, WIDTH - 1 if idx == 0 else HEIGHT - 1) 
+          used = False
+          for cell in snake.body:
+            if cell[idx] == pos[idx]:
+              used = True
+              break
+          if not used:
+            break
+        idx += 1;
+      if self.position[pos[1]][pos[0]] == 0:
+        break;
+      else:
+        print(self.position, "\n")
+
+    self.position[pos[1]][pos[0]] = 1
+
+    return pos[0], pos[1]
+
+
+
+class Game():
+  def __init__(self):
+    self.snake = Snake()
+    pygame.init()
+    self.screen = pygame.display.set_mode([700, 700])
+    cur = (3, int(HEIGHT/2))
+    apples = Apples()
+
+    running = True
+    self.draw_board()
+    self.draw_snake(self.snake.body)
+    mov = (+1, 0)
+    while running:
+      if apples.count < APPLES_COUNT:
+        print("add_apple")
+        new_apple_pos = apples.add_apple(self.snake)
+        self.draw_apple(new_apple_pos)
+      for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              running = False
+          if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+              mov = (-1, 0)
+            elif event.key == pygame.K_RIGHT:
+              mov = (+1, 0)
+            elif event.key == pygame.K_DOWN:
+              mov = (0, +1)
+            elif event.key == pygame.K_UP:
+              mov = (0, -1)
+      new_pos = (self.snake.head[0] + mov[0], self.snake.head[1] + mov[1])
+      if new_pos[0] >= WIDTH or new_pos[1] >= HEIGHT or new_pos[0] < 0 or new_pos[1] < 0:
+        break
+      grow = False
+      if apples.position[new_pos[1]][new_pos[0]]:
+        grow = True
+        apples.remove_apple(new_pos)
+      else:
+        self.remove_snake_tail()
+      self.snake.move(mov, grow)
+      self.add_snake_head()
+      pygame.display.flip()
+      pygame.time.wait(100)
+
+    # Done! Time to quit.
+    pygame.quit()
+
+  def remove_snake_tail(self):
+    self.draw_board_cell(self.snake.tail)
+  
+  def add_snake_head(self):
+    x, y = self.snake.head 
+    pygame.draw.rect(self.screen, BLUE, (x*SIZE,y*SIZE, SIZE,SIZE))
+
+  def draw_board_cell(self, pos):
+    x, y = pos
+    pygame.draw.rect(self.screen, DARK_GREEN if (x + y * WIDTH) % 2 == 0 else LIGHT_GREEN, (x*SIZE,y*SIZE, SIZE,SIZE))
+
+  def draw_board(self):
+    turn = 0;
+    for y in range(HEIGHT):
+      for x in range(WIDTH):
+        pygame.draw.rect(self.screen, DARK_GREEN if turn % 2 == 0 else LIGHT_GREEN, (x*SIZE,y*SIZE, SIZE,SIZE))
+        turn += 1;
+
+  def draw_snake(self, snake: list):
+    for pos in snake:
+      x, y = pos;
+      pygame.draw.rect(self.screen, BLUE, (x*SIZE+5,y*SIZE+5, SIZE-10,SIZE-10))
+
+  def draw_apple(self, pos):
+    x, y = pos
+    pygame.draw.circle(self.screen, RED, (x*SIZE+SIZE/2,y*SIZE+SIZE/2), SIZE-SIZE/2-4)
+    pygame.draw.rect(self.screen, APPLE_GREEN, (x*SIZE+25,y*SIZE, SIZE-30,SIZE-40))
+
+
+Game()
